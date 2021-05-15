b13d8df154f1475642bdd619d0023fa1b3ca3061
Athul Thomas <thomasathul@gmail.com>
2021-05-15 22:20:23 +0530

    Update delay.cpp

--- delay.cpp
+++ delay.cpp
@@ -8,26 +8,26 @@ int main()
 	cout<<"Enter delay time (sec)"<<"\n";
 	cin>>delay;
 
-    //Looping till interupt
+        //Looping till interupt
 	while(1)
 	{
 	cout<<"Delay "+to_string(delay)+ "s"<<endl;
 
 	    //Displaying delay sequence
 	   for(int i=1;i<=delay;i++)
-       {
-    	if(i*100/delay!=100)
-    	{
-    	    cout<<float((i*100)/delay)<<"...";
+             {
+    	       if(i*100/delay!=100)
+    	        {
+    	          cout<<float((i*100)/delay)<<"...";
+                }
+               else
+                {
+        	  cout<<float((i*100)/delay)<<endl;
+                }
+                //Sleep for 1 sec
+               sleep(1);
+             }
         }
-        else
-        {
-        	cout<<float((i*100)/delay)<<endl;
-        }
-        //Sleep for 1 sec
-    	sleep(1);
-       }
-    }
     
  }
 
